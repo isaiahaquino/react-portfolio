75c1c7f3e0e74f5cd93dab2035109c4a3b24ca92
Isaiah <67047480+isaiahaquino@users.noreply.github.com>
2023-03-03 12:41:57 -0800

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Isaiah Aquino Portfolio',
+  title: 'Isaiah Aquino | Front-End Developer',
   description: 'My Front-End Web Developer Portfolio',
+  keywords: ['Next.js', 'React', 'JavaScript', 'Isaiah', 'Aquino', 'Front-end', 'Portfolio'],
+  creator: 'Isaiah Aquino',
+  icons: {
+    icon: [{url: '/sunset_icon_246575.ico'}],
+  },
 }
 
 export default function RootLayout({
